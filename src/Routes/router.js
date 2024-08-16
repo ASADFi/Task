6a7e9e4a56786d1526f5bf90536c72561f35f7d3
Asad Meer <meerasads636@gmail.com>
2024-08-16 19:02:55 +0500

    initial commit

--- src/Routes/router.js
+++ src/Routes/router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 var multer = require("multer");
 
 const controller = require("../Controllers/controller");
-const check_auth = require("../Middleware/check_auth");
+
 
 router.get("/holiday", controller.getHolidays);
 router.get("/countries", controller.getcountries);
