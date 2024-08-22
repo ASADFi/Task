cb02184eb1321396db51efb7c6e67f1428fb55f9
Asad Meer <meerasads636@gmail.com>
2024-08-22 15:11:52 +0500

    setting header

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin,X-Request-With,context-Type,Accept,Authorization,x-freepik-api-key,Accept"
+    "Origin,X-Request-With,Content-Type,Accept,Authorization,x-freepik-api-key"
   );
 
   res.header(
